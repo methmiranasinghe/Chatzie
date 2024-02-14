2f79f072cb0b2eab98385823af83300b5302d7b8
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2024-02-14 14:59:14 +0530

    Initial Commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  console.log("signupUser");
+};
+
+export const login = () => {
+  console.log("loginUser");
+};
+
+export const logout = () => {
+  console.log("logoutUser");
+};
