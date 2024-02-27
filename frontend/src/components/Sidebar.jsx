51ce7a510a9a61fe1a6cc887513e2a36cc990c12
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2024-02-27 18:22:59 +0530

    Sidebar added

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,15 +1,15 @@
+//import Conversation from "./Conversation";
+import Conversations from "./Conversations";
 import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
   return (
-    <div>
-      <SearchInput/>
-      <div className="divider px-3">
-        {/* <Conversation/>
-        <LogoutButton/> */}
-      </div>
+    <div className="border-r border-slate-500 p-4 flex flex-col">
+      <SearchInput />
+      <div className="divider px-3"></div>
+      <Conversations />
+      {/* <LogoutButton /> */}
     </div>
   );
-}
-
+};
 export default Sidebar;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../../components/Sidebar";
 
 const Home = () => {
   return (
-    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-600 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
      <Sidebar/>
      {/* <MessageContainer/> */}
     </div>
@@ -11,3 +11,5 @@ const Home = () => {
 }
 
 export default Home;
+//  <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+//       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0"></div>
