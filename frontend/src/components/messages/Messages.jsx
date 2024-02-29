28ea4cda06236ec69bb004129f507982edf4bc69
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2024-03-01 01:18:26 +0530

    Search function added

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,17 +1,35 @@
-import useGetMessages from '../../hooks/useGetMessages';
-import MessageSkeleton from '../skeletons/MessageSkeleton.jsx';
-import Message from './Message.jsx';
+import { useEffect, useRef } from "react";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
+import Message from "./Message";
+//import useListenMessages from "../../hooks/useListenMessages";
 
 const Messages = () => {
-  const {messages,loading} = useGetMessages();
-  console.log("messages:",messages);
+	const { messages, loading } = useGetMessages();
+	//useListenMessages();
+	const lastMessageRef = useRef();
 
-  return (
-    <div className='px-4 flex-1 overflow-auto'>
-      {loading && [...Array(3)].map((_, idx)=> <MessageSkeleton key={idx}/>)}
+	useEffect(() => {
+		setTimeout(() => {
+			lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+		}, 100);
+	}, [messages]);
 
-    </div>
-  );
-}
+	return (
+		<div className='px-4 flex-1 overflow-auto'>
+			{!loading &&
+				messages.length > 0 &&
+				messages.map((message) => (
+					<div key={message._id} ref={lastMessageRef}>
+						<Message message={message} />
+					</div>
+				))}
 
+			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+			{!loading && messages.length === 0 && (
+				<p className='text-center'>Send a message to start the conversation</p>
+			)}
+		</div>
+	);
+};
 export default Messages;
--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -5,7 +5,7 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
 	const { selectedConversation, setSelectedConversation } = useConversation();
 
 	const isSelected = selectedConversation?._id === conversation._id;
-	//const { onlineUsers } = useSocketContext();
+  //const { onlineUsers } = useSocketContext();
 	//const isOnline = onlineUsers.includes(conversation._id);
 
 	return (
