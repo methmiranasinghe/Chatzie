f9e3bddc063126768d366596bf4a5cbaf515458c
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2024-03-01 23:10:08 +0530

    Final Implementation

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,15 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
-
+import path from "path";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import {app, server} from "./socket/socket.js"
 
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
 dotenv.config()
 
 app.use(express.json());// to parse the incoming requests with json payloads(from req.body)
@@ -21,6 +24,11 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages", messageRoutes);
 app.use("/api/users",userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
